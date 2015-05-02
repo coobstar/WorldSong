9627ee01572a267a57c3bff9d147a1c330322a6e
Carl Majeau <carl.majeau@gmail.com>
2015-05-02 17:54:37 -0400

    boom

--- example/streamserver.js
+++ example/streamserver.js
@@ -49,8 +49,7 @@ var
 //  Speaker = require('speaker'),
 
   Mixer = require('../index.js'),
-  wav = require('wav'),
-  lame = require('lame')
+  wav = require('wav')
   ;
 
 /*
@@ -69,49 +68,6 @@ var mixer = new Mixer({
 
 //mixer.pipe(speaker);
 
-/*
- * Decode mp3 and add the stream as mixer input:
- */
-
-var file0 = fs.createReadStream('example0.mp3');
-
-var decoder0 = new lame.Decoder();
-var mp3stream0 = file0.pipe(decoder0);
-
-decoder0.on('format', function (format) {
-  console.log(format);
-
-  mp3stream0.pipe(mixer.input({
-    sampleRate: format.sampleRate,
-    channels: format.channels,
-    bitDepth: format.bitDepth
-  }));
-
-});
-
-/*
- * Decode mp3 and add the stream as mixer input:
- */
-
-var file1 = fs.createReadStream('example1.mp3');
-
-var decoder1 = new lame.Decoder();
-var mp3stream1 = file1.pipe(decoder1);
-
-decoder1.on('format', function (format) {
-  console.log(format);
-
-  mp3stream1.pipe(mixer.input({
-    sampleRate: format.sampleRate,
-    channels: format.channels,
-    bitDepth: format.bitDepth
-  }));
-
-});
-
-/*
- * Decode mp3 and add the stream as mixer input:
- */
 
 var file2 = fs.createReadStream('test2.wav');
 var reader = new wav.Reader();
